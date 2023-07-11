92c28a8e51cda32282f63083412b8799372918bd
Daria <d-step@mail.ru>
2023-07-11 16:19:47 +0700

    11.07

--- MyProject/api_basic/serializers.py
+++ MyProject/api_basic/serializers.py
@@ -25,5 +25,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        # fields = ['id', 'title', 'author']
+        # fields = ['id', 'title', 'author', 'email']
         fields = '__all__'
